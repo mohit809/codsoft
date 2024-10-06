29ea24eff627402c6426bb85f85c28c2e12685f2
MOHIT KUMAR SHARMA <72223875+mohit809@users.noreply.github.com>
2024-10-06 12:36:51 -0700

    Add files via upload

---
+++ email/src/page/index.jsx
@@ -0,0 +1,326 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='flex-row'>
+        <div className='img-icons' />
+        <div className='group' />
+        <div className='rectangle'>
+          <div className='img-icons-1' />
+          <div className='img-icons-2' />
+          <span className='search'>Search</span>
+        </div>
+        <span className='shopiecart'>shopiecart</span>
+      </div>
+      <div className='flex-row-da'>
+        <div className='fashionotsav' />
+        <div className='rectangle-3'>
+          <div className='fashionotsav-4'>
+            <span className='ads'>ads</span>
+          </div>
+        </div>
+        <div className='fashionotsav-5' />
+      </div>
+      <div className='frame'>
+        <div className='group-6'>
+          <span className='men-fashion'>Men Fashion</span>
+          <div className='flex-row-7'>
+            <div className='group-8'>
+              <div className='group-9'>
+                <div className='group-a'>
+                  <div className='rectangle-b' />
+                  <div className='rectangle-c' />
+                  <span className='buy'>BUY</span>
+                </div>
+                <div className='group-d'>
+                  <div className='rectangle-e' />
+                  <div className='group-f'>
+                    <div className='rectangle-10' />
+                    <div className='rectangle-11' />
+                    <span className='buy-12'>BUY</span>
+                  </div>
+                  <div className='rectangle-13' />
+                  <span className='buy-14'>BUY</span>
+                </div>
+              </div>
+              <div className='fashionable-male-fashion-designer' />
+              <div className='download-removebg-preview' />
+              <div className='cotton-short-sleeved-t-shirt' />
+            </div>
+            <div className='group-15'>
+              <div className='img-icons-16' />
+              <span className='number-33'>33</span>
+            </div>
+            <div className='group-17'>
+              <div className='img-icons-18' />
+              <span className='number-40'>40</span>
+            </div>
+            <div className='group-19'>
+              <div className='img-icons-1a' />
+              <span className='span'>34</span>
+            </div>
+          </div>
+        </div>
+        <div className='group-1b'>
+          <span className='tshirt'>Tshirt</span>
+          <div className='flex-row-1c'>
+            <div className='group-1d'>
+              <div className='group-1e'>
+                <div className='group-1f'>
+                  <div className='rectangle-20' />
+                  <div className='rectangle-21' />
+                  <span className='buy-22'>BUY</span>
+                </div>
+                <div className='group-23'>
+                  <div className='rectangle-24' />
+                  <div className='group-25'>
+                    <div className='rectangle-26' />
+                    <div className='rectangle-27' />
+                    <span className='buy-28'>BUY</span>
+                  </div>
+                  <div className='rectangle-29' />
+                  <span className='buy-2a'>BUY</span>
+                </div>
+              </div>
+              <div className='lovepik-com-fashionable-male' />
+              <div className='download-removebg-preview-2b' />
+              <div className='pngimg-com-tshirt' />
+              <div className='lovepik-com-cotton-short-sleeved' />
+            </div>
+            <div className='group-2c'>
+              <div className='img-icons-2d' />
+              <span className='span-2e'>13</span>
+            </div>
+            <div className='group-2f'>
+              <div className='img-icons-30' />
+              <span className='span-31'>19</span>
+            </div>
+            <div className='group-32'>
+              <div className='img-icons-33' />
+              <span className='span-34'>15</span>
+            </div>
+          </div>
+        </div>
+        <div className='group-35'>
+          <span className='wedding-dresses'>Wedding Dresses</span>
+          <div className='flex-row-de'>
+            <div className='group-36'>
+              <div className='group-37'>
+                <div className='group-38'>
+                  <div className='fres-removebg-preview' />
+                  <div className='rectangle-39' />
+                  <div className='rectangle-3a' />
+                  <span className='buy-3b'>BUY</span>
+                </div>
+                <div className='group-3c'>
+                  <div className='rectangle-3d' />
+                  <div className='group-3e'>
+                    <div className='velvet-sherwani' />
+                    <div className='rectangle-3f' />
+                    <div className='rectangle-40' />
+                    <span className='buy-41'>BUY</span>
+                  </div>
+                  <div className='indo-western-shervani' />
+                  <div className='rectangle-42' />
+                  <span className='buy-43'>BUY</span>
+                </div>
+              </div>
+            </div>
+            <div className='group-44'>
+              <div className='icons' />
+              <span className='number-323'>323</span>
+            </div>
+            <div className='group-45'>
+              <div className='icons-46' />
+              <span className='number-480'>480</span>
+            </div>
+            <div className='group-47'>
+              <div className='icons-48' />
+              <span className='number-334'>334</span>
+            </div>
+          </div>
+        </div>
+        <div className='group-49'>
+          <span className='men-fashion-4a'>Men Fashion</span>
+          <div className='flex-row-ec'>
+            <div className='group-4b'>
+              <div className='group-4c'>
+                <div className='group-4d'>
+                  <div className='rectangle-4e' />
+                  <div className='rectangle-4f' />
+                  <span className='buy-50'>BUY</span>
+                </div>
+                <div className='group-51'>
+                  <div className='rectangle-52' />
+                  <div className='group-53'>
+                    <div className='rectangle-54' />
+                    <div className='rectangle-55' />
+                    <span className='buy-56'>BUY</span>
+                  </div>
+                  <div className='rectangle-57' />
+                  <span className='buy-58'>BUY</span>
+                </div>
+              </div>
+              <div className='male-fashion-designer' />
+              <div className='download-removebg' />
+              <div className='cotton-t-shirt' />
+            </div>
+            <div className='group-59'>
+              <div className='icons-5a' />
+              <span className='number-33-5b'>33</span>
+            </div>
+            <div className='group-5c'>
+              <div className='icons-5d' />
+              <span className='span-5e'>40</span>
+            </div>
+            <div className='div-group'>
+              <div className='div-img-icons' />
+              <span className='span-5f'>34</span>
+            </div>
+          </div>
+        </div>
+        <div className='div-group-60'>
+          <span className='span-men-fashion'>Men Fashion</span>
+          <div className='div-flex-row-f'>
+            <div className='div-group-61'>
+              <div className='div-group-62'>
+                <div className='div-group-63'>
+                  <div className='div-rectangle' />
+                  <div className='div-rectangle-64' />
+                  <span className='span-buy'>BUY</span>
+                </div>
+                <div className='div-group-65'>
+                  <div className='div-rectangle-66' />
+                  <div className='div-group-67'>
+                    <div className='div-rectangle-68' />
+                    <div className='div-rectangle-69' />
+                    <span className='span-buy-6a'>BUY</span>
+                  </div>
+                  <div className='div-rectangle-6b' />
+                  <span className='span-buy-6c'>BUY</span>
+                </div>
+              </div>
+              <div className='div-lovepik-com-fashionable-male-fashion-designer' />
+              <div className='div-download-removebg-preview' />
+              <div className='div-lovepik-com-cotton-short-sleeved-t-shirt' />
+            </div>
+            <div className='div-group-6d'>
+              <div className='div-img-icons-6e' />
+              <span className='span-6f'>33</span>
+            </div>
+            <div className='div-group-70'>
+              <div className='div-img-icons-71' />
+              <span className='span-72'>40</span>
+            </div>
+            <div className='div-group-73'>
+              <div className='div-img-icons-74' />
+              <span className='span-75'>34</span>
+            </div>
+          </div>
+        </div>
+        <div className='div-group-76'>
+          <span className='span-men-fashion-77'>Men Fashion</span>
+          <div className='div-flex-row-ea'>
+            <div className='div-group-78'>
+              <div className='div-group-79'>
+                <div className='div-group-7a'>
+                  <div className='div-rectangle-7b' />
+                  <div className='div-rectangle-7c' />
+                  <span className='span-buy-7d'>BUY</span>
+                </div>
+                <div className='div-group-7e'>
+                  <div className='rectangle-7f' />
+                  <div className='group-80'>
+                    <div className='rectangle-81' />
+                    <div className='rectangle-82' />
+                    <span className='buy-83'>BUY</span>
+                  </div>
+                  <div className='rectangle-84' />
+                  <span className='buy-85'>BUY</span>
+                </div>
+              </div>
+              <div className='fashionable-male-fashion-designer-86' />
+              <div className='download-removebg-preview-87' />
+              <div className='cotton-short-sleeved-t-shirt-88' />
+            </div>
+            <div className='group-89'>
+              <div className='icons-8a' />
+              <span className='full-stack'>33</span>
+            </div>
+            <div className='group-8b'>
+              <div className='icons-8c' />
+              <span className='full-stack-8d'>40</span>
+            </div>
+            <div className='group-8e'>
+              <div className='icons-8f' />
+              <span className='full-stack-90'>34</span>
+            </div>
+          </div>
+        </div>
+        <div className='summer-t-shirts' />
+      </div>
+      <div className='frame-91'>
+        <div className='frame-92'>
+          <div className='group-93'>
+            <div className='group-94'>
+              <div className='icons-95' />
+              <span className='shopiecart-96'>shopiecart</span>
+            </div>
+          </div>
+          <span className='sector-laksmi-nagar-new-delhi'>
+            12/3/ Sector 160 laksmi Nagar
+            <br />
+            New Delhi xxxxxx
+            <br />
+            India
+          </span>
+        </div>
+        <div className='frame-97'>
+          <div className='get-to-know-us'>
+            <span className='get-to-know-us-98'>
+              Get to Know Us
+              <br />
+            </span>
+            <span className='about-shopiecart'>
+              About Shopiecart
+              <br />
+            </span>
+            <span className='careers'>
+              Careers
+              <br />
+            </span>
+            <span className='press-releases'>
+              Press Releases
+              <br />
+            </span>
+            <span className='shopiecart-99'>Shopiecart</span>
+            <span className='science'>
+              
+              Science
+              <br />
+            </span>
+          </div>
+          <div className='connect-with-us'>
+            <span className='connect-with-us-9a'>
+              Connect with Us
+              <br />
+            </span>
+            <span className='facebook'>
+              Facebook
+              <br />
+            </span>
+            <span className='twitter'>
+              Twitter
+              <br />
+            </span>
+            <span className='instagram'>Instagram</span>
+          </div>
+        </div>
+      </div>
+      <span className='copy-right'>@ Copy right</span>
+      <div className='rectangle-9b' />
+    </div>
+  );
+}
